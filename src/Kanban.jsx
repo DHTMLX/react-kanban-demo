6945ff6c8123281234d1711db4371859dc510ecc
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-06-25 13:16:47 +0400

    [dev] refactoring

--- src/Kanban.jsx
+++ src/Kanban.jsx
@@ -1,18 +1,29 @@
 import { useEffect, useRef } from "react";
-import { Kanban } from "@dhx/trial-kanban";
+import { Kanban, Toolbar } from "@dhx/trial-kanban";
 import "@dhx/trial-kanban/dist/kanban.css";
 
-export default function KanbanView(props) {
-  let container = useRef();
+export default function KanbanComponent(props) {
+  let kanban_container = useRef();
+  let toolbar_container = useRef();
 
   useEffect(() => {
-    new Kanban(container.current, {
+    const kanban = new Kanban(kanban_container.current, {
       columns: props.columns,
       cards: props.cards,
+      // other configuration properties
     });
 
-    return () => (container.current.innerHTML = "");
+    
+    new Toolbar(toolbar_container.current, {
+      api: kanban.api,
+      // other configuration properties
+    });
+
+    return () => (kanban_container.current.innerHTML = "", toolbar_container.current.innerHTML = "");
   }, []);
 
-  return <div ref={container} style={{ width: "100%", height: "100%" }}></div>;
+  return  <div>
+            <div ref={toolbar_container}></div>
+            <div ref={kanban_container} style={{ width: "100%", height: "100%" }}></div>
+          </div>
 }
